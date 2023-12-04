5389976292c38ac7a30cdcbe6a9b80460d5b624f
Rasul Khamzin <rasul.khamzinnn@gmail.com>
2023-12-04 19:50:26 +0500

    02

---
+++ 02 Array.ts
@@ -0,0 +1,22 @@
+let list: number[] = [1, 2, 3];
+
+let listOne: Array<number> = [2, 34];
+
+let y: [string, number] = ['yelf', 1]
+
+let z: Array<any> = [1, false, 'not'];
+
+enum Directions{
+    Up= 2,
+    Down= 4,
+    Left= 6,
+    Right,
+}
+
+Directions.Up;
+Directions.Down;
+Directions.Left;
+Directions.Right;
+
+type Name = string;
+let id: Name;
