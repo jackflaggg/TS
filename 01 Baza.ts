0ee6de04c7483dedf37619cbca74db2b8678c4b3
Rasul Khamzin <rasul.khamzinnn@gmail.com>
2023-12-04 19:39:34 +0500

    Initial commit

---
+++ 01 Baza.ts
@@ -0,0 +1,12 @@
+let isCompleted : boolean = false;
+isCompleted = true;
+
+const decimal: number = 23;
+
+const nameVar: string = 'str';
+const u: undefined = undefined;
+const n: null = null;
+
+const greetUser = (): void => {
+    console.log('trie')
+}
